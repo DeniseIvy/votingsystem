9367fa8503f460281ba30bf5ee5475f4dfbff31d
Nicolei Esperida <putikmo@ymail.com>
2019-03-13 23:01:57 +0800

    New login design

--- src/login.java
+++ src/login.java
@@ -1,7 +1,7 @@
 
+import java.awt.Color;
 import javax.swing.JOptionPane;
-import java.awt.event.*;
-import java.awt.*;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -34,12 +34,14 @@ public class login extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jPanel4 = new javax.swing.JPanel();
-        untitle = new javax.swing.JLabel();
+        lbl_user = new javax.swing.JLabel();
         usernamef = new javax.swing.JTextField();
-        passtitle = new javax.swing.JLabel();
+        lbl_pass = new javax.swing.JLabel();
         passwordf = new javax.swing.JPasswordField();
         loginb = new javax.swing.JButton();
         signupb = new javax.swing.JButton();
+        jSeparator1 = new javax.swing.JSeparator();
+        jSeparator2 = new javax.swing.JSeparator();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -53,26 +55,44 @@ public class login extends javax.swing.JFrame {
 
         jPanel4.setBackground(new java.awt.Color(72, 99, 142));
 
-        untitle.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
-        untitle.setForeground(new java.awt.Color(168, 188, 221));
-        untitle.setText("USERNAME");
+        lbl_user.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        lbl_user.setForeground(new java.awt.Color(255, 255, 255));
+        lbl_user.setText("USERNAME");
 
-        usernamef.setBackground(new java.awt.Color(133, 156, 191));
-        usernamef.setFont(new java.awt.Font("Tahoma", 0, 22)); // NOI18N
+        usernamef.setBackground(new java.awt.Color(72, 99, 142));
+        usernamef.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        usernamef.setForeground(new java.awt.Color(255, 255, 255));
         usernamef.setBorder(null);
+        usernamef.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                usernamefFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                usernamefFocusLost(evt);
+            }
+        });
         usernamef.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 usernamefActionPerformed(evt);
             }
         });
 
-        passtitle.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
-        passtitle.setForeground(new java.awt.Color(168, 188, 221));
-        passtitle.setText("PASSWORD");
+        lbl_pass.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        lbl_pass.setForeground(new java.awt.Color(51, 51, 51));
+        lbl_pass.setText("PASSWORD");
 
-        passwordf.setBackground(new java.awt.Color(133, 156, 191));
-        passwordf.setFont(new java.awt.Font("Tahoma", 0, 22)); // NOI18N
+        passwordf.setBackground(new java.awt.Color(72, 99, 142));
+        passwordf.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        passwordf.setForeground(new java.awt.Color(255, 255, 255));
         passwordf.setBorder(null);
+        passwordf.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                passwordfFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                passwordfFocusLost(evt);
+            }
+        });
         passwordf.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 passwordfActionPerformed(evt);
@@ -92,53 +112,47 @@ public class login extends javax.swing.JFrame {
         signupb.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
         signupb.setForeground(new java.awt.Color(133, 156, 191));
         signupb.setText("SIGN UP");
-        signupb.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                signupbActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
+                .addGap(201, 201, 201)
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(loginb, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(signupb, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 232, Short.MAX_VALUE))
+            .addGroup(jPanel4Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
-                        .addComponent(signupb)
-                        .addGap(37, 37, 37))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(jPanel4Layout.createSequentialGroup()
-                            .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(untitle)
-                                .addComponent(passtitle))
-                            .addGap(415, 415, 415))
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(usernamef, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(passwordf, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGap(45, 45, 45)))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
-                        .addComponent(loginb)
-                        .addGap(190, 190, 190))))
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(lbl_pass)
+                    .addComponent(lbl_user)
+                    .addComponent(usernamef, javax.swing.GroupLayout.DEFAULT_SIZE, 321, Short.MAX_VALUE)
+                    .addComponent(jSeparator1)
+                    .addComponent(passwordf)
+                    .addComponent(jSeparator2))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
-                .addGap(165, 165, 165)
-                .addComponent(signupb)
-                .addGap(29, 29, 29)
-                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(untitle, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(usernamef, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(52, 52, 52)
-                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(passwordf, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(passtitle))
-                .addGap(28, 28, 28)
+                .addGap(161, 161, 161)
+                .addComponent(lbl_user, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(usernamef, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(23, 23, 23)
+                .addComponent(lbl_pass)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(passwordf, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(42, 42, 42)
                 .addComponent(loginb)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(signupb)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -194,11 +208,12 @@ public class login extends javax.swing.JFrame {
         if (username.equals("admin")&& password.equals("1234"))
         {
             JOptionPane.showMessageDialog(null,"Login Successful");
+            dispose();
             new admin().setVisible(true);
         }
         else 
         {
-           JOptionPane.showMessageDialog(null,"ERROR");
+           JOptionPane.showMessageDialog(null,"Login Failed");
            usernamef.setText(null);
            passwordf.setText(null);
         }
@@ -209,9 +224,25 @@ public class login extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_passwordfActionPerformed
 
-    private void signupbActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_signupbActionPerformed
-        new SignUp().setVisible(true);
-    }//GEN-LAST:event_signupbActionPerformed
+    private void passwordfFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_passwordfFocusGained
+        lbl_pass.setForeground(new Color(240,240,240));
+        jSeparator2.setBackground(new Color(149,165,166));
+    }//GEN-LAST:event_passwordfFocusGained
+
+    private void passwordfFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_passwordfFocusLost
+        lbl_pass.setForeground(new Color(33,33,33));
+        jSeparator2.setBackground(new Color(33,33,33));
+    }//GEN-LAST:event_passwordfFocusLost
+
+    private void usernamefFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_usernamefFocusGained
+        lbl_user.setForeground(new Color(240,240,240));
+        jSeparator1.setBackground(new Color(149,165,166));
+    }//GEN-LAST:event_usernamefFocusGained
+
+    private void usernamefFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_usernamefFocusLost
+        lbl_user.setForeground(new Color(33,33,33));
+        jSeparator1.setBackground(new Color(33,33,33));
+    }//GEN-LAST:event_usernamefFocusLost
 
     /**
      * @param args the command line arguments
@@ -253,13 +284,13 @@ public class login extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JLabel lbl_pass;
+    private javax.swing.JLabel lbl_user;
     private javax.swing.JButton loginb;
-    private javax.swing.JLabel passtitle;
     private javax.swing.JPasswordField passwordf;
     private javax.swing.JButton signupb;
-    private javax.swing.JLabel untitle;
     private javax.swing.JTextField usernamef;
     // End of variables declaration//GEN-END:variables
-
 }
-
